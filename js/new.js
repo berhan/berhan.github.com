17f393b404d4f86af35eea2ba609af5a96ad7f69
Berhan Senyazar <berhansenyazar@gmail.com>
2012-08-02 15:53:37 +0300

    remove my login screen

--- js/new.js
+++ js/new.js
@@ -7,23 +7,26 @@ function authGame(){
 	$('body').append('<div id="authDialog"></div>');
 	$('#authDialog').append('<h1>Sise Cevirmece\'ye Hosgeldin</h1>');
 	$('#authDialog').append('<br>');
-	$('#authDialog').append('<p>Lutfen id giriniz</p>');
-	$('#authDialog').append('<input id="authIdInput" type="text"></input>');
+	//$('#authDialog').append('<p>Lutfen id giriniz</p>');
+	//$('#authDialog').append('<input id="authIdInput" type="text"></input>');
 	$('#authDialog').append('<button id="authGiris">Giris</button>');
-	$("#authIdInput").keyup(function(event){
-	if(event.keyCode == 13){
-		$("#authGiris").click();
-	}
-	});
+	// $("#authIdInput").keyup(function(event){
+	// 	if(event.keyCode == 13){
+	// 		$("#authGiris").click();
+	// 	}
+	// });
 	$('#authGiris').click(function(){
-		var id = $('#authIdInput').val();
-		if(fb.auth(id)){
-			player = new Player(id);
-			init();
-		}else{
-			$('#authDialog').append('<p>Gecersiz!</p>');
-		}
+		//var id = $('#authIdInput').val();
+		FB.login();
 	});
+}
+
+function login(){
+	init();
+}
+
+function logout(){
+
 }
 
 function init(){
